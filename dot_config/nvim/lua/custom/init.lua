968b106d92fa299481dddde24c7a052fb9c189dc
Phil Kulak <phil@kulak.us>
2023-02-01 14:03:02 -0800

    Stop using the system clipboard for everything.

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -1,5 +1,11 @@
+-- set up formatters for XML and JSON
 local options = {noremap = true, silent = true}
 
 vim.api.nvim_set_keymap('n', '=j', ':%!python -m json.tool<CR>:set syntax=json<CR>', options)
 
 vim.api.nvim_set_keymap('n', '=x', ':%!python -c \'import sys;import xml.dom.minidom;s=sys.stdin.read();print(xml.dom.minidom.parseString(s).toprettyxml())\'<CR>:set syntax=xml<CR>', options)
+
+-- use our own options
+local opt = vim.opt
+
+opt.clipboard = "unnamed"
