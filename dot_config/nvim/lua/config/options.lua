adbebf6c61a58bb34d2a60e776b94f8a9cf6ee4b
Phil Kulak <phil@kulak.us>
2023-03-13 20:58:00 -0700

    Move to LazyVim.

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,15 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- set up formatters for XML and JSON
+local options = { noremap = true, silent = true }
+
+vim.api.nvim_set_keymap("n", "=j", ":%!python -m json.tool<CR>:set syntax=json<CR>", options)
+
+vim.api.nvim_set_keymap(
+  "n",
+  "=x",
+  ":%!python -c 'import sys;import xml.dom.minidom;s=sys.stdin.read();print(xml.dom.minidom.parseString(s).toprettyxml())'<CR>:set syntax=xml<CR>",
+  options
+)
