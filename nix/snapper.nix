5aeed8fb6c0d2e11d63c7b3d206475ca607731e1
Phil Kulak <phil@kulak.us>
2023-06-25 22:49:37 -0700

    Update snapper.nix

--- nix/snapper.nix
+++ nix/snapper.nix
@@ -6,19 +6,10 @@
 
     services.snapper.configs = {
       home = {
-        subvolume = "/home";
-
-        extraConfig = ''
-          ALLOW_GROUPS="users"
-          TIMELINE_CREATE="yes"
-          TIMELINE_CLEANUP="yes"
-          TIMELINE_MIN_AGE="1800"
-          TIMELINE_LIMIT_HOURLY="12"
-          TIMELINE_LIMIT_DAILY="7"
-          TIMELINE_LIMIT_WEEKLY="4"
-          TIMELINE_LIMIT_MONTHLY="0"
-          TIMELINE_LIMIT_YEARLY="0"
-        '';
+        SUBVOLUME = "/home";
+        ALLOW_GROUPS = ["users"];
+        TIMELINE_CLEANUP = true;
+        TIMELINE_CREATE = true;
       };
     };
   };
