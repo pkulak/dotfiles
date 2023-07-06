2dd85f9d5ced6a70d45e788b7ed84498e8675cb5
Phli Kulak <phil@kulak.us>
2023-07-05 23:30:14 -0700

    Don't depend on a channel being present.

--- nix/modules/hosts/x1.nix
+++ nix/modules/hosts/x1.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
-{
+let
+  nixos-hardware = import (fetchTarball("https://github.com/NixOS/nixos-hardware/archive/master.tar.gz")) {};
+in {
   imports = [ <nixos-hardware/lenovo/thinkpad/x1/6th-gen> ]
   networking.hostName = "x1";
   programs.light.enable = true;
